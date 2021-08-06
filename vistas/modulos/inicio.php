c8eba094a15c48ad50e2cb4c41bf5d65432d7747
angel-alvarez-guzman <angel.alvarez.guzman0@gmail.com>
2021-08-06 13:08:24 -0500

    Se agregó carrusel

--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -260,3 +260,91 @@
    </div>
  </div>
 </section>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <div class="block">
+             <div class="row">
+               <div class="col-md-5">
+                 <div class="user">
+                   <div class="image">
+                     <img src="vistas/img/plantilla/user-1.png" alt="">
+                   </div>
+                   <div class="info">
+                     <h2 class="user-name">Alan</h2>
+                     <h4>Cliente</h4>
+                   </div>
+                 </div>
+               </div>
+               <div class="col-md-6">
+                 <div class="content">
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro incidunt maiores ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint optio recusandae blanditiis nisi. Incidunt, rem alias corporis aspernatur recusandae eos corrupti fugiat aut sit non, porro necessitatibus fuga autem. Eius.</p>
+                 </div>
+               </div>
+             </div>
+           </div>
+    </div>
+    <div class="carousel-item">
+    <div class="block">
+             <div class="row">
+               <div class="col-md-5">
+                 <div class="user">
+                   <div class="image">
+                     <img src="vistas/img/plantilla/user-2.png" alt="">
+                   </div>
+                   <div class="info">
+                     <h2 class="user-name">Angel</h2>
+                     <h4>Cliente</h4>
+                   </div>
+                 </div>
+               </div>
+               <div class="col-md-6">
+                 <div class="content">
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro incidunt maiores ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint optio recusandae blanditiis nisi. Incidunt, rem alias corporis aspernatur recusandae eos corrupti fugiat aut sit non, porro necessitatibus fuga autem. Eius.</p>
+                 </div>
+               </div>
+             </div>
+           </div>
+    </div>
+    <div class="carousel-item">
+    <div class="block">
+             <div class="row">
+               <div class="col-md-5">
+                 <div class="user">
+                   <div class="image">
+                     <img src="vistas/img/plantilla/user-3.png" alt="">
+                   </div>
+                   <div class="info">
+                     <h2 class="user-name">Juan</h2>
+                     <h4>Cliente</h4>
+                   </div>
+                 </div>
+               </div>
+               <div class="col-md-6">
+                 <div class="content">
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro incidunt maiores ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint optio recusandae blanditiis nisi. Incidunt, rem alias corporis aspernatur recusandae eos corrupti fugiat aut sit non, porro necessitatibus fuga autem. Eius.</p>
+                 </div>
+               </div>
+             </div>
+           </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<br>
+<br>
+<br>
